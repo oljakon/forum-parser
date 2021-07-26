72179484b227392037f04fd39ba359786e83e9d9
Olga Kondrashova <ol.kondraschowa@gmail.com>
2021-07-27 00:34:48 +0300

    connect via tor

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import requests
+
+
+class ForumParser:
+
+    def get_tor_session(self):
+        session = requests.session()
+        session.proxies = {'http': 'socks5h://127.0.0.1:9050',
+                           'https': 'socks5h://127.0.0.1:9050'}
+        return session
+
+
 def main():
-    pass
+    parser = ForumParser()
+    session = parser.get_tor_session()
+    print(session.get('https://miped.ru/f/whats-new/posts').text)
 
 
 if __name__ == '__main__':
